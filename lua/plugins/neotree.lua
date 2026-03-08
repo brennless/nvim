f25e6dce8549b22c48e4636a17476c3d2a87ed4e
Alois Maier <alois.maier@bordeaux-inp.fr>
2026-03-08 15:04:56 +0100

    modif neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -17,10 +17,55 @@ return {
             },
         },
         config = function()
-            require("neo-tree").setup({
+            require("neo-tree").setup(
+            {
                 close_if_last_window = true, 
                 enable_git_status = true,
                 enable_diagnostics = true,
+                default_component_configs = {
+                    indent = {
+                        indent_size = 2,
+                        padding = 1,
+                        with_markers = true,
+                        indent_marker = "│",
+                        last_indent_marker = "└",
+                        highlight = "NeoTreeIndentMarker",
+                        with_expanders = true, -- Affiche les flèches pour les dossiers
+                        expander_collapsed = "",
+                        expander_expanded = "",
+                        expander_highlight = "NeoTreeExpander",
+                    },
+                    icon = {
+                        folder_closed = "",
+                        folder_open = "",
+                        folder_empty = "",
+                        default = "",
+                        highlight = "NeoTreeFileIcon"
+                    },
+                    modified = {
+                        symbol = "*",
+                        highlight = "NeoTreeModified",
+                    },
+                    name = {
+                        trailing_slash = false,
+                        use_git_status_colors = true,
+                        highlight = "NeoTreeFileName",
+                    },
+                    git_status = {
+                        symbols = {
+                            -- Icônes Git modernes et minimalistes
+                            added     = "+",
+                            modified  = "~",
+                            deleted   = "-",
+                            renamed   = "R",
+                            untracked = "?",
+                            ignored   = "I",
+                            unstaged  = "U",
+                            staged    = "S",
+                            conflict  = "",
+                        }
+                    },
+                },
             })
         end,
         lazy = false, 
